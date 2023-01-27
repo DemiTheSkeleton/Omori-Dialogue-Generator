f6aaaa9237df7683bf030e5a1ab495f001d78d59
Ransu_ll <lancekyao@gmail.com>
2023-01-27 22:04:18 +1100

    Add extra comments and format all files

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,11 @@
-const normalFont = new FontFace("OMORI_MAIN", "url(./css/fonts/OMORI_GAME.ttf)");
-const disturbedFont = new FontFace("OMORI_DISTURBED", "url(./css/fonts/OMORI_GAME2.ttf)");
+const normalFont = new FontFace(
+  "OMORI_MAIN",
+  "url(./css/fonts/OMORI_GAME.ttf)"
+);
+const disturbedFont = new FontFace(
+  "OMORI_DISTURBED",
+  "url(./css/fonts/OMORI_GAME2.ttf)"
+);
 // const disturbedFont = new FontFace("OMORI_MAIN", "url(./css/fonts/OMORI_GAME.ttf)");
 document.fonts.add(normalFont);
 document.fonts.add(disturbedFont);
@@ -9,16 +15,17 @@ console.log("Fonts loaded");
 // This is needed to prevent issues with toDataURL DOM issues ("tainted canvas")
 const img_dialogue = new Image();
 img_dialogue.crossOrigin = "anonymous";
-img_dialogue.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAmAAAABwCAYAAAC5IsxiAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAI1SURBVHhe7dmxDcMwDABBMfvv7Ci2q6SNv7oDBFAbPMhZax37AQAQOQNsu34AADxqZtbrngEAiAgwAIDYzwnysxYDAOB/vlvLBgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAIDb7Hdv1AwDgUTNjAwYAUBNgAACx8wR5jQAAPG+tN+QPEde1pEOrAAAAAElFTkSuQmCC";
+img_dialogue.src =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAmAAAABwCAYAAAC5IsxiAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAI1SURBVHhe7dmxDcMwDABBMfvv7Ci2q6SNv7oDBFAbPMhZax37AQAQOQNsu34AADxqZtbrngEAiAgwAIDYzwnysxYDAOB/vlvLBgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAICbAAABiAgwAIDb7Hdv1AwDgUTNjAwYAUBNgAACx8wR5jQAAPG+tN+QPEde1pEOrAAAAAElFTkSuQmCC";
 
 const img_portrait = new Image();
 img_portrait.crossOrigin = "anonymous";
-img_portrait.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHIAAAByCAYAAACP3YV9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAE+SURBVHhe7dTBDQIxDADBmP57hnDHCxogqxkpkvNe2bPWeu7H4a6Q2/3jSDOzHp+ZwwkZ8XNa32vK//tuZiMjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjZr/ndv840szYyAohI67Teo+ca60X0ncR2/R13icAAAAASUVORK5CYII=";
+img_portrait.src =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHIAAAByCAYAAACP3YV9AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAE+SURBVHhe7dTBDQIxDADBmP57hnDHCxogqxkpkvNe2bPWeu7H4a6Q2/3jSDOzHp+ZwwkZ8XNa32vK//tuZiMjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjhIwQMkLICCEjZr/ndv840szYyAohI67Teo+ca60X0ncR2/R13icAAAAASUVORK5CYII=";
 
 // TODO: make this modular
 
 function renderCanvas(idFrame, idDownload) {
-
   const frame = document.getElementById(idFrame);
   let canvas = frame.getElementsByTagName("canvas")[0];
   canvas.style.display = "block";
@@ -35,13 +42,12 @@ function renderCanvas(idFrame, idDownload) {
   // Load dialogue box
   ctx.drawImage(img_dialogue, 0, 118);
 
-
   // Load character name details
   let charName = document.getElementsByClassName("char-name")[0].textContent;
   ctx.font = "28px OMORI_MAIN";
 
   // Load character name box
-  let a = ctx.measureText(charName)
+  let a = ctx.measureText(charName);
   let lengthOfCharName = a.width;
   let fullRectangleWidth = lengthOfCharName + 24;
 
@@ -78,7 +84,9 @@ function renderCanvas(idFrame, idDownload) {
     for (let lineNo = 0; lineNo < splitText.length; lineNo++) {
       console.log(lineNo);
       if (lineNo >= 3) {
-        alert('Only a maximum of three lines can fit. The fourth line and so has not been rendered.');
+        alert(
+          "Only a maximum of three lines can fit. The fourth line and so has not been rendered."
+        );
         break;
       }
       let line = splitText[lineNo];
@@ -96,7 +104,7 @@ function renderCanvas(idFrame, idDownload) {
           }
         }
       }
-      context.fillText(line, 18, yBase + (29 * lineNo), maxLineLength);
+      context.fillText(line, 18, yBase + 29 * lineNo, maxLineLength);
     }
   }
 
@@ -110,7 +118,7 @@ function renderCanvas(idFrame, idDownload) {
   // refuse to render.
   ctx.fillStyle = "white";
   if (document.getElementById("toggle_disturbed_0").checked) {
-    disturbedFont.load().then(function() {
+    disturbedFont.load().then(function () {
       ctx.font = "28px OMORI_DISTURBED";
       insertDialogue(ctx, dialogue);
       changeDownloadLink();
@@ -120,8 +128,6 @@ function renderCanvas(idFrame, idDownload) {
     insertDialogue(ctx, dialogue);
     changeDownloadLink();
   }
-
-
 
   /*
   Credits - giving credit where credit is due.
--- js/script.js
+++ js/script.js
@@ -2,69 +2,71 @@ function toggleInfo(idToggle, idLink) {
   let extendedDisplay = document.getElementById(idToggle);
   let toggleLink = document.getElementById(idLink);
 
-  if (extendedDisplay.style.display == 'none' || extendedDisplay.style.display == '') {
-    extendedDisplay.style.display = 'block';
-    toggleLink.text = 'How to use? (show less)';
+  if (
+    extendedDisplay.style.display == "none" ||
+    extendedDisplay.style.display == ""
+  ) {
+    extendedDisplay.style.display = "block";
+    toggleLink.text = "How to use? (show less)";
   } else {
-    extendedDisplay.style.display = 'none';
-    toggleLink.text = 'How to use? (show more)';
+    extendedDisplay.style.display = "none";
+    toggleLink.text = "How to use? (show more)";
   }
-  console.log("toggled display!")
-};
+  console.log("toggled display!");
+}
 
 function togglePortrait(idCheckbox, idPortrait) {
   let checkbox = document.getElementById(idCheckbox);
   let image = document.getElementById(idPortrait);
 
   if (checkbox.checked) {
-    image.parentElement.style.visibility = 'visible'; // The black border
-    image.style.display = 'inherit'; // The image itself
+    image.parentElement.style.visibility = "visible"; // The black border
+    image.style.display = "inherit"; // The image itself
   } else {
-    image.parentElement.style.visibility = 'hidden'; // The black border
-    image.style.display = 'none'; // The image itself
+    image.parentElement.style.visibility = "hidden"; // The black border
+    image.style.display = "none"; // The image itself
   }
-};
+}
 
 function clearPortrait(identifier) {
   let image = document.getElementById(identifier);
-  image.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
-  image.nextElementSibling.value = null
-};
-
+  image.src =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+  image.nextElementSibling.value = null;
+}
 
 function toggleText(idCheckbox, idTextarea) {
   let checkbox = document.getElementById(idCheckbox);
   let textarea = document.getElementById(idTextarea);
 
   if (checkbox.checked) {
-    textarea.style.fontFamily = 'OMORI_DISTURBED';
+    textarea.style.fontFamily = "OMORI_DISTURBED";
   } else {
-    textarea.style.fontFamily = 'OMORI_MAIN';
+    textarea.style.fontFamily = "OMORI_MAIN";
   }
-};
+}
 
 function clearFrame(identifier) {
   let frame = document.getElementById(identifier);
   // The below needs to be refactored at some point, OH GOD
-  let download = frame.getElementsByTagName('a')[0];
+  let download = frame.getElementsByTagName("a")[0];
   // End of refactor section
-  let image = frame.getElementsByTagName('img')[0];
-  let canvas = frame.getElementsByTagName('canvas')[0];
+  let image = frame.getElementsByTagName("img")[0];
+  let canvas = frame.getElementsByTagName("canvas")[0];
 
   frame.reset();
   image.src = "https://via.placeholder.com/106/000000?text=click+me!";
   canvas.style.display = "none";
   download.style.display = "none";
-
-};
+}
 
 function displayPortrait(event, identifier) {
   // Adapted from https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript, by Jiwan Thapa
   let image = document.getElementById(identifier);
-  image.src = URL.createObjectURL(event.target.files[0])
-};
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
 
 function displayDownload(identifier) {
   let downloadButton = document.getElementById(identifier);
-  downloadButton.style.display = 'inline-block';
-};
+  downloadButton.style.display = "inline-block";
+}
